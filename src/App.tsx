a7fbdee914e7f2fe271c6b932000887a50a8e172
Vadim Smagin <magenvadim@gmail.com>
2024-03-15 17:46:37 +0200

    #3 Header component has added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import './App.css'
 import { useState } from 'react'
 import GlobalStyle1 from "./Styles/theme1"
 import GlobalStyle2 from "./Styles/theme2"
 import GlobalStyle3 from "./Styles/theme3"
 
+import { Container, Header, WrapperSwitch, Switch, SwitcherContainer, Switcher } from "./Styles/App.style"
+
 function App() {
 
   return (
-    <></>
+    <>
+      <Container>
+
+        <Header>
+          Calc
+          <WrapperSwitch>
+            theme
+
+            <Switch>
+              <div>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+              </div>
+              <SwitcherContainer>
+
+                <Switcher/>
+                
+              </SwitcherContainer>
+            </Switch>
+
+
+          </WrapperSwitch>
+        </Header>
+
+      </Container>
+    </>
   )
 }
 
---
+++ src/Styles/App.style.ts
@@ -0,0 +1,65 @@
+import styled, {createGlobalStyle} from "styled-components";
+
+type Theme = {
+    theme: string;
+}
+
+
+export const Container = styled.div`
+    max-width: 500px;
+    min-width: 300px;
+    margin: 0 auto;
+    user-select: none;
+
+    @media screen and (min-width: 450px){
+        min-width: 340px;
+    }
+`
+export const Flex = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center
+`
+export const Header = styled(Flex)`
+    justify-content: space-between;
+    color: var(--white-text);
+    font-size: 1.2rem;
+    background-color: lightblue;
+`
+
+export const WrapperSwitch = styled(Flex)`
+    width: 115px;
+    font-size: .85rem;
+    align-items: flex-end;
+    justify-content: space-between;
+    background-color: bisque;
+`
+export const Switch = styled.div`
+    font-size: .9rem;
+    flex: 0.75;
+    justify-content: center;
+    align-items: center;
+    background-color: lemonchiffon;
+    
+    div:first-of-type{
+        width: 80%;
+        font-size: .65rem;
+        font-weight: 100;
+        display: flex;
+        justify-content: space-between;
+        margin: 0 auto;
+    }
+`
+export const SwitcherContainer = styled.div`
+    background-color: var(--toggle-background);
+    width: 100%;
+    min-height: 17px;
+    border-radius: 2rem;
+    position: relative;
+    z-index: 20;
+    cursor: pointer;    
+`
+
+export const Switcher = styled.div<Theme>`
+    width: 11px;
+`
